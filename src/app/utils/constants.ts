d6f25833b8caa2bb4f9e7cfc4e945dd6c1b55bdd
Sai Surya Teja Maddikonda <sai.maddikonda@gmail.com>
2020-04-25 13:57:41 -0400

    Update constants

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -2,9 +2,9 @@ export const APP_NAME = 'CoronaTimes'
 
 export const WELCOME_MESSAGE = 'Please fill out this form and let us know how you are surviving the pandemic.\n' +
   'By clicking the appropriate button at the bottom of the page, you can:\n' +
-  '1 <b>SUBMIT</b> the form: All fields are mandatory, and your submission will be <b>public</b>.\n' +
-  '2 <b>SAVE AS DRAFT</b>: you can skip one or more fields, but your progress will be <b>public</b>.\n' +
-  '3 <b>EXIT</b> the app whenever you want; your progress will be saved and will be <b>private</b>.';
+  '1. <b>SUBMIT</b> the form: All fields are mandatory, and your submission will be <b>public</b>.\n' +
+  '2. <b>SAVE AS DRAFT</b>: you can skip one or more fields, but your progress will be <b>public</b>.\n' +
+  '3. <b>EXIT</b> the app whenever you want; your progress will be saved and will be <b>private</b>.';
 
 export const FULL_DISCLOSURE = 'This is a personal/hobby project. Please read the README file ' +
   'in the Github repository by following the link in the footer to know more about the project.\n' +
