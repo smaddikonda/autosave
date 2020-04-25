1e0af5744ebf0f9d977d0e8cfc96ecea8b8108f5
Sai Surya Teja Maddikonda <sai.maddikonda@gmail.com>
2020-04-25 06:58:23 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormComponent } from './form/form.component';
+import { DraftsComponent } from './drafts/drafts.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FormComponent,
+    DraftsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
